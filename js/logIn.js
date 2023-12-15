4be12d2f277b23afdd667c7b025ae0cf01ca80e1
testusuario <jonyxt1063@gmail.com>
2023-12-14 21:08:31 -0600

    first try

---
+++ js/logIn.js
@@ -0,0 +1,58 @@
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const button = document.querySelector('.button');
+let userInformation = JSON.parse(localStorage.getItem('information'));
+
+
+
+if(document.querySelector('.button')){
+    button.addEventListener('click', loginValidation);
+}
+
+
+function loginValidation(){
+    const error = document.querySelector('#form p');
+    if(error){
+        error.remove();
+    };
+    let userFound = false;
+
+    userInformation.forEach(value => {
+        if (email.value === value.email || email.value === value.name) {
+            email.classList.remove('error');
+            userFound = true;
+
+            if (password.value === value.password) {
+                password.classList.remove('error');
+                // window.location.href = 'dashboard.html'
+                update(value);
+            } else {
+                return errorMessage('incorrect password', password);
+            }
+        }
+    });
+
+    if (!userFound) {
+        return errorMessage("this user doesn't exist", email)
+    }
+    
+}
+
+function update(value){
+    const updateUser = userInformation.map(user =>{
+        if(user.email === value.email || user.numberAccount === value.numberAccount){
+            return {...user, active: true}; 
+        }
+            return user;
+    });
+    localStorage.setItem('information', JSON.stringify(updateUser));
+}
+
+
+function errorMessage(message, input){
+    let pError = document.createElement('p')
+    pError.textContent = message;
+    pError.style.color = 'red';
+    input.insertAdjacentElement('afterend', pError);
+    input.classList.add('error')
+}
---
+++ js/signUp.js
@@ -0,0 +1,104 @@
+const userName = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const button = document.querySelector('.button');
+const form = document.querySelector('#form');
+let userInformation = JSON.parse(localStorage.getItem('information')) || [];
+
+userName.addEventListener('keyup', signUpValidation);
+email.addEventListener('keyup', signUpValidation);
+password.addEventListener('keyup', signUpValidation);
+confirmPassword.addEventListener('keyup', signUpValidation);
+button.addEventListener('click', createAccount)
+
+buttonDisabled();
+function buttonDisabled(){
+    button.disabled = true;
+    button.id = 'button'
+}
+
+
+function signUpValidation(e){
+    const error = document.querySelector('#form p');
+    if(error){
+        error.remove();
+    };
+    if(e.target.type === 'text'){
+        let user = userName.value.trimStart();
+        if(user.length <= 0){
+            errorMessage('invalid user name', userName);
+        }else{
+            userName.classList.remove('error');
+            addValidation(userName);
+        }
+
+    }else if(e.target.type === 'email'){
+        if(email.value.includes('@') && email.value.includes('.') && email.value.length > 6){
+            email.classList.remove('error');
+            addValidation(email);
+        }else{
+            errorMessage('invalid email', email);
+        }
+
+    }else if(e.target.type === 'password'){
+        if(password.value.length <= 5){
+            errorMessage('minimum is 6 characters', password);
+        }else if(confirmPassword.value === ''){
+            confirmPassword.classList.remove('error');
+            addValidation(password);
+        }
+        else if(password.value !== confirmPassword.value){
+            errorMessage("password doesn't match", confirmPassword);
+        }else{
+            confirmPassword.classList.remove('error');
+            password.classList.remove('error');
+            addValidation(confirmPassword);
+        }
+    }
+    if(userName.classList.value === 'validated' && email.classList.value === 'validated' && confirmPassword.classList.value === 'validated'){
+        button.removeAttribute('id');
+        button.disabled = false;
+    }
+}
+
+function errorMessage(message, input){
+    const pError = document.createElement('p');
+    pError.textContent = message;
+    pError.style.color = 'red'
+    input.insertAdjacentElement('afterend', pError);
+
+    input.classList.add('error');
+}
+function addValidation(input){
+    input.classList.add('validated')
+}
+
+function createAccount(){
+    let information = {
+        name: userName.value,
+        email: email.value,
+        password: password.value,
+        numberAccount: createAccountNumber(),
+        active: false,
+    }
+    Swal.fire({
+        title: "Welcome!",
+        text: "Your account has been successfully created!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500
+    });
+    setTimeout(() => {
+        window.location.href = "index.html"
+    }, 1500);
+    userInformation.push(information)
+    localStorage.setItem('information', JSON.stringify(userInformation));
+}
+function createAccountNumber(){
+    let accountNumber = [];
+    for (let i = 0; i <= 10; i++) {
+        accountNumber.push(Math.round(Math.random() * 9))
+    }
+    return accountNumber.join('')
+}
